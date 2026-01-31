7408a944e71a7f0b03f3f7795ce162c0044ec1df
Ivan-Arvizu <ivan.arvicamp@gmail.com>
2026-01-31 11:49:44 -0600

    add day01 sql template

---
+++ leetcode/day01.sql
@@ -0,0 +1,18 @@
+-- Day 01: SELECT / WHERE / ORDER BY / LIMIT
+-- Goal: build fundamentals for analytics/quant interviews.
+
+-- Template per problem:
+-- Problem:
+-- Approach:
+-- Notes:
+
+-- Problem 1:
+-- Approach:
+-- Notes:
+SELECT 1;
+
+-- Problem 2:
+SELECT 1;
+
+-- Problem 3:
+SELECT 1;
